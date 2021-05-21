e6ec3656a71e444ca8bd27ecbaca5de5ef12aa8f
seirin1807 <84576445+seirin1807@users.noreply.github.com>
2021-05-21 15:51:35 +0300

    Add files via upload

---
+++ Proj/proj.js
@@ -0,0 +1,16 @@
+
+var countDownDate = new Date("May 31, 2021 15:00:00").getTime();
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("time").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("time").innerHTML = "Время вышло";
+  }
+}, 1000)
